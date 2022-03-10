5f32c5dc8eaea5a12ab48f3a920a4e721d977275
Любомир Лалов <ldlalov@hotmail.com>
2022-03-10 09:32:28 +0200

    Add project files.

---
+++ 01. Reverse Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            StringBuilder sb = new StringBuilder();
+            while ((input = Console.ReadLine()) != "end")
+            {
+                char[] cmd = input.ToCharArray();
+                char[] newCmd = cmd.Reverse().ToArray();
+                sb.Append(newCmd);
+                Console.WriteLine($"{input} = {sb}");
+                sb.Clear();
+            }
+        }
+    }
+}
